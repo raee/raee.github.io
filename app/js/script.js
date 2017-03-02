7584f4ef95c1297d9dc4d832be830bd7c33e0e92
Rae <798404374@qq.com>
2017-03-03 01:28:03 +0800

    finish app index.html

--- app/js/script.js
+++ app/js/script.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     wow.init();
 
     //MagnificPopup
-    $('.image-link').magnificPopup({type: 'image'});
+    $('.image-link').magnificPopup({type: 'image', removalDelay: 300, mainClass: 'mfp-fade'});
 
 
     // OwlCarousel N1
@@ -43,13 +43,29 @@ $(document).ready(function () {
         itemsDesktopSmall: [979, 3]
     });
 
-    // OwlCarousel N2
-    $("#owl-demo-1").owlCarousel({
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
+    $("#owl-cnblogs").owlCarousel({
+        autoPlay: 3000,
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
     });
+
+    // OwlCarousel N2
+    // $("#owl-hgxwp").owlCarousel({
+    //     autoPlay: 3000,
+    //     navigation: false, // Show next and prev buttons
+    //     slideSpeed: 300,
+    //     paginationSpeed: 400,
+    //     singleItem: true
+    // });
+
+    // $("#owl-nfzd").owlCarousel({
+    //     autoPlay: 3000,
+    //     navigation: false, // Show next and prev buttons
+    //     slideSpeed: 300,
+    //     paginationSpeed: 400,
+    //     singleItem: true
+    // });
 
     //SmothScroll
     $('a[href*=#]').click(function () {
@@ -67,7 +83,7 @@ $(document).ready(function () {
 
     //Subscribe
     var morphButton = document.querySelector('.morph-button');
-    if(morphButton) {
+    if (morphButton) {
         new UIMorphingButton(morphButton);
     }
     // for demo purposes only
@@ -78,4 +94,155 @@ $(document).ready(function () {
     });
 
 });
+
+var scrollTo = function (selector) {
+
+    var $target = $(selector);
+    if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, 600);
+        return false;
+    }
+}
+
+var initVideo = function () {
+    var sphVideo = $('#video_sph')[0];
+
+    sphVideo.onplay = function () {
+
+        // 停止音乐
+        var bgAudio = $('#bg-audio')[0];
+        if (!bgAudio.paused) {
+            bgAudio.pause();
+            stopRotation();
+        }
+
+
+        $('#video_play').hide();
+        $('#video_paused').show();
+
+    };
+
+    sphVideo.onpause = function () {
+        $('#video_play').show();
+        $('#video_paused').hide();
+    };
+
+    $('#video_sph').bind({
+        mouseover: function () {
+            toggleVideoState();
+        },
+        mousemove: function () {
+            toggleVideoState();
+        },
+        mouseout: function () {
+            toggleVideoState(true);
+        },
+        click: function () {
+            toggleVideoState();
+        }
+    });
+    $('#video_play').bind({
+        click: function () {
+            playVideo(sphVideo);
+        }
+    });
+    $('#video_paused').bind({
+        click: function () {
+            playVideo(sphVideo);
+        }
+    });
+};
+
+var videoStateTimeout;
+
+var toggleVideoState = function (isOut) {
+
+    clearTimeout(videoStateTimeout);
+
+    var video = $("#video_sph")[0];
+
+    $('.video-controller').show();
+
+    // if (video.played.length <= 0 || video.paused) {
+    //     $('#video_play').show();
+    //     $('#video_paused').hide();
+    // } else {
+    //     $('#video_play').hide();
+    //     $('#video_paused').show();
+    // }
+
+    videoStateTimeout = setTimeout(function () {
+        if (video.paused) return;
+        $('.video-controller').fadeOut();
+    }, 3000);
+}
+
+var playVideo = function () {
+
+    var video = $("#video_sph")[0];
+
+
+    if (video.played.length <= 0 || video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+    scrollTo('#sph-video');
+
+}
+
+var playVideoClick = function () {
+    $("#video_sph")[0].play();
+}
+
+var playMusic = function () {
+    var bgAudio = $('#bg-audio')[0];
+    if (bgAudio.paused) {
+        bgAudio.play();
+    } else {
+        bgAudio.pause();
+    }
+}
+
+var musicRotation = function () {
+
+    $(".bg-music").rotate({
+        angle: getRotateAngle($(this)),
+        animateTo: 360,
+        callback: musicRotation,
+        easing: function (x, t, b, c, d) {        // t: current time, b: begInnIng value, c: change In value, d: duration
+            return c * (t / d) + b;
+        },
+        bind: {
+            click: function () {
+                playMusic();
+            },
+            mouseover: function () {
+                stopRotation();
+            },
+            mouseout: function () {
+                var bgAudio = $('#bg-audio')[0];
+                if (bgAudio.paused) {
+                    stopRotation();
+                    $(this).rotate(0);
+                    return;
+                }
+                musicRotation();
+            }
+        }
+    });
+}
+
+var stopRotation = function () {
+    $(".bg-music").stopRotate();
+}
+
+var getRotateAngle = function (e) {
+    var rotate = $(e).getRotateAngle();
+    if (!rotate || rotate <= 0 || rotate >= 360) return 0;
+    return rotate;
+}
+
 
